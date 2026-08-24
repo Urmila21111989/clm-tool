ea17848f4e95ee3b27cbe6a97d19b26f91174d18
Urmila21111989 <urmila.naikwade21@gmail.com>
2026-08-24 12:40:28 +0530

    Update index.js

--- server/src/index.js
+++ server/src/index.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(express.json());
 app.use('/api/contracts', contractsRouter);
 app.use('/api/search', searchRouter);
-app.use(express.static(path.join(__dirname, '..', '..', 'public')));
+app.use(express.static(path.join(__dirname, '..', 'public')));
 
 const PORT = process.env.PORT || 3000;
 
